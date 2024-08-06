c88d3dab9962ab34ccc5a3b120f32159afdaec8a
shalininoogala <103871648+shalininoogala@users.noreply.github.com>
2024-08-05 22:42:42 -1200

    Add files via upload
    docker

---
+++ front1.py
@@ -0,0 +1,201 @@
+import streamlit as st
+import requests
+from gtts import gTTS
+from io import BytesIO
+from streamlit_mic_recorder import speech_to_text
+from streamlit.runtime.scriptrunner import get_script_run_ctx
+import speech_recognition as sr
+import asyncio
+backendURL="https://658e-103-191-91-82.ngrok-free.app"
+session_id = get_script_run_ctx().session_id
+async def delete_chat(id):
+    # to delete a chat from backend
+    # delete_chat_url = "http://127.0.0.1:8000/deleteChat/"
+    delete_chat_url=backendURL+"/deleteChat/"
+    data = {
+        "session_id": session_id+str(id)
+    }
+    try:
+        response = requests.post(delete_chat_url, json=data)
+        if response.status_code == 200:
+            st.success("New chat ")
+                # to delete the chats in the front-end ie streamlit 
+            if id in st.session_state.messages:
+                del st.session_state.messages[id]
+
+            # to delete the chat_uid of the chat from chat_uids list.
+            if id in st.session_state.chat_uids:
+                st.session_state.chat_uids.remove(id)
+
+            # to update the screen once the chat is deleted
+            st.rerun()
+
+        else:
+            st.error("Error Deleting chat")
+    except Exception as e:
+
+        st.error("Error connecting server, Pls. try again.")
+        return None
+
+
+# text to voice function using gTTS library
+def text_to_speech_and_display(text):
+    tts = gTTS(text=text, lang='en')
+    audio_fp = BytesIO()
+    tts.write_to_fp(audio_fp)
+    audio_fp.seek(0)
+    st.audio(audio_fp, format="audio/mp3")
+
+
+def display_chat():
+    
+    # getting the messages only from the current chat id.
+    msg=st.session_state.messages.get(st.session_state.curr_uid, [])
+
+    # displaying the messages in latest to oldest message order
+    for i in range(len(msg)-1,-1,-1):
+        if msg[i]["content"] != "":
+            if msg[i]["role"]=="assistant":
+                with st.chat_message(msg[i-1]["role"]):
+                    st.markdown(msg[i-1]["content"])
+                with st.chat_message(msg[i]["role"]):
+                    #displaying audio of the result
+                    st.markdown(msg[i]["content"])
+                    text_to_speech_and_display(msg[i]["content"])
+
+# Calling the caBuddy endpoint of API to get LLM response by session_id + current chat
+# using session_id + current chat id to keep each chat unique
+async def llm_call(prompt):
+    # url = "http://127.0.0.1:8000/caBuddy/"
+    url=backendURL+"/caBuddy/"
+    request_params = {
+        "message": prompt,
+        "session_id": session_id+str(st.session_state.curr_uid)
+    }
+    try:
+        response = requests.post(url=url, json=request_params)
+        return response
+    except:
+        st.error("Error reaching server. Please Try again later")
+        return None
+
+async def main():
+
+    # Initialize recognizer class                                       
+    r = sr.Recognizer()
+    st.set_page_config(page_title="CA AI Assistant")
+
+    if "chat_uids" not in st.session_state:
+        #chat_uids is a list of active chat_ids
+        st.session_state.chat_uids = []
+
+        #messages is a dictionary structure storing list of messages with chat_uid as the key
+        st.session_state.messages = {}
+
+        # curr_uid is the current chat_uid 
+        st.session_state.curr_uid = 1
+
+        #total_ids -> total ids generated, as we add new chat total_id is incremented,
+        # used to assign unique chat_uid 
+        st.session_state.total_ids=1
+
+    #Side Bar
+    st.sidebar.title("")
+    if st.sidebar.button("New Chat ➕"):
+        # to increment total ids
+        st.session_state.total_ids += 1
+
+        #initialising the current chat id 
+        st.session_state.curr_uid=st.session_state.total_ids
+
+
+    # To display the recent chats with their first user message and delete btn.
+    for i in reversed(st.session_state.chat_uids):
+        with st.sidebar.container():
+            col1, col2 = st.columns([4, 1])
+
+        # to get the first user prompt from the chat
+        firstQuestion=st.session_state.messages.get(i,[])[0]["content"]
+
+        #to limit the character limit of the chat title to 15 characters only.
+        if len(firstQuestion)>15:
+            firstQuestion=firstQuestion[0:15]+".."
+
+        # every button is recognised by the chat id and their column no.
+        #Chat Button
+        if (col1.button(firstQuestion,key=str(i)+"c1")):
+            st.session_state.curr_uid=i
+
+        #Delete chat Button
+        if col2.button("🗑",key=str(i)+"c2"):
+            await delete_chat(i)
+
+
+
+    st.title("CA Assistant")
+
+
+
+
+    #initialising transcribed_text string
+    transcribed_text=""
+
+    # Layout for text input and microphone
+    text_input_col, mic_col = st.columns([4, 1])  # Adjust proportions as needed
+
+    # voice input column
+    with mic_col:
+        # to take speech input and transcribe it using speech recognition library
+        text = speech_to_text(language='en', just_once=True, key='STT',start_prompt="🎙️", stop_prompt="🔴")
+        transcribed_text=text
+
+    #text input column
+    with text_input_col:
+        prompt = st.chat_input("Ask your CA Buddy")
+
+
+    if prompt:
+        #Initialising a new chat
+        if st.session_state.curr_uid not in st.session_state.messages:
+            st.session_state.messages[st.session_state.curr_uid] = []
+            st.session_state.chat_uids.append(st.session_state.curr_uid)
+
+        # appeding user messages
+        st.session_state.messages[st.session_state.curr_uid].append({"role": "user", "content": prompt})
+        
+        #using llm_call function to get response to user prompt
+        response =  await llm_call(prompt)
+        # if response is None then it didn't get any response from LLM
+        if response is not None:
+            # if response status code is 200 then it's succesful else it's an error
+            if response.status_code == 200:
+                st.session_state.messages[st.session_state.curr_uid].append({"role": "assistant", "content": response.text})
+            else:
+                st.write("Error: Please Try again.", response)
+
+
+    if transcribed_text:
+        #Initialising a new chat
+        if st.session_state.curr_uid not in st.session_state.messages:
+            st.session_state.messages[st.session_state.curr_uid] = []
+            st.session_state.chat_uids.append(st.session_state.curr_uid)
+
+        # appending user messages
+        st.session_state.messages[st.session_state.curr_uid].append({"role": "user", "content": transcribed_text})
+    
+        #using llm_call function to get response to user prompt
+    
+        response =  await llm_call(transcribed_text)
+        transcribed_text = ""
+
+        # if response status code is 200 then it's succesful else it's an error
+        if response.status_code == 200:
+            st.session_state.messages[st.session_state.curr_uid].append({"role": "assistant", "content": response.text})
+        else:
+            st.write("Error, Please Try again.:", response)
+
+    # to display chats
+    display_chat()
+
+if __name__ == '__main__':
+    asyncio.run(main())
